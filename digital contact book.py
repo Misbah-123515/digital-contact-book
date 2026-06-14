e5fa341ab0933a2d9b73dc463a867fc6322aa126
Misbah-123515 <misbafatima21@gmail.com>
2026-06-14 23:08:25 +0530

    Add files via upload

---
+++ digital contact book.py
@@ -0,0 +1,15 @@
+contacts = {}
+
+# Add 3 contacts
+for i in range(3):
+    name = input("Enter contact name: ")
+    phone = input("Enter phone number: ")
+    contacts[name] = phone
+
+# Search for a contact
+search_name = input("\nEnter name to search: ")
+
+if search_name in contacts:
+    print("Phone Number:", contacts[search_name])
+else:
+    print("Contact not found.")
